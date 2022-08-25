975732504fe481c94539c1d11b579f004fe5ab29
dot-1915 <dea.dorothy@gmail.com>
2022-08-25 14:34:06 -0700

    adding back in code from previous work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
+import React from "react";
+import Weather from "./Weather.js";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This App was Coded by Deanna Roney and is
+          <a
+            href="https://github.com/dot-1915/react-weather-2.0"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open Sourced
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
-
-
