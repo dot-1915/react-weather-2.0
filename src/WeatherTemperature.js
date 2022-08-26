fb31e57f23185494f70d77d0b7ac119bf827a09b
dot-1915 <dea.dorothy@gmail.com>
2022-08-26 11:19:30 -0700

    added live data for conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -24,9 +24,10 @@ export default function WeatherTemperature(props) {
       </div>
     );
   } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="temperature">{Math.round(fahrenheit)}</span>
         <span className="unit">
           <a href="/" onClick={convertToCelsius}>
             {" "}
